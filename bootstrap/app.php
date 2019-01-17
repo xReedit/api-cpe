d0066d857498f244910bf9dc96e275e2a74c8aa8
Marcelo <xReedit@gmail.com>
2019-01-17 16:05:09 -0500

    cors 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@
 |
 */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: *');
+// header('Access-Control-Allow-Headers: *');
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
